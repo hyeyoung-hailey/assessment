c64534092cfc463ae6e1e37cfd76d90b2a03d4c7
hyeyoung-hailey <hyeyounghaileysim@gmail.com>
2023-01-20 03:32:27 +0900

    Edit unnecessary code line

--- src/main/java/com/torthful/assessment/helloworld/HelloWorldController.java
+++ src/main/java/com/torthful/assessment/helloworld/HelloWorldController.java
@@ -1,8 +1,11 @@
 package com.torthful.assessment.helloworld;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 public class HelloWorldController {
 
@@ -15,5 +18,7 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldBean() throws Exception {
         return new HelloWorldBean("Hello World");
     }
+
+
 }
 
